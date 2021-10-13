f878efb5bb8e69f0b7e16e266c0fe8d5c4785d59
Tchilo <tchiloross@gmail.com>
2021-10-13 19:36:31 +0200

    Add footer to home page and add styling to the footer and add heart image

--- src/interactiveAPI.js
+++ src/interactiveAPI.js
@@ -1,6 +1,24 @@
+const appID = JSON.parse(localStorage.getItem('dataBase'));
+const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/'
+
+if (localStorage.getItem('dataBase') === null) {
+  fetch(url, {
+      method: 'Post',
+      headers: {
+        'content-type': 'application/json; charset=UTF-8'
+      }
+
+    })
+    .then(response => response.text())
+    .then(data => {
+      localStorage.setItem('dataBase', JSON.stringify(data))
+    })
+
+}
+
 const createLikes = async (artID) => {
   const data = JSON.stringify({ item_id: artID });
-  const resp = await fetch(,{
+  const resp = await fetch(url += ,{
     method: 'POST',
     headers: {
       'Content-type': 'application/json; Charset=UTF-8',
