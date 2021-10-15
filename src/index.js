a4425dea9affe6fc9bed24579871414772eb7792
Niko Escobal <niko.escobal@gmail.com>
2021-10-15 18:10:35 +0800

    Add function to display images dynamically in home page

--- src/index.js
+++ src/index.js
@@ -10,12 +10,32 @@ const getImages = async () => {
       id: image['id'],
       image_id: image['image_id'],
       title: image['title'],
-      date: image['date_start']
+      date: image['date_start'],
+      artist: image['artist_title']
     }
   })
-  imageString = imageString.filter(image => image.image_id !== null && image.title !== null);
-
+  imageString.filter(image => image.image_id !== null && image.title !== null).map(images_url => `<article
+  class="flex justify-center w-full h-full p-3 flex-col space-y-2 border-double border-4 border-blue-300 bg-white">
+  <h2 class="font-bold font-raleway text-lg">${images_url.title},
+    ${images_url.date}</h2>
+  <h3 class="px-3 font-extralight font-raleway">${images_url.artist}</h3>
+  <img src="https://www.artic.edu/iiif/2/${images_url.image_id}/full/843,/0/default.jpg"
+    alt="image of artwork">
+  <figure class="flex justify-between px-3 space-x-4">
+    <figcaption class="flex py-3 space-x-6 text-base w-full font-nunito">
+      <img src="/src/heart-filled.png" alt="heart icon">&nbsp; Like
+      <img src="/src/comment.png" alt="comment icon">&nbsp; Comment
+    </figcaption>
+  </figure>
+</article>`).join('');
   console.log(imageString);
+  const div = document.createElement('div');
+  div.innterHTML = imageString;
+  const artContainer = document.getElementById('art-container');
+  artContainer.appendChild(div);
+  document.body.appendChild(artContainer);
 }
+
+
 
 getImages();
