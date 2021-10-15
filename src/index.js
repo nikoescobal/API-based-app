4c1c95a04e8dfed5d23dfa9c74edf6a51af8910e
Niko Escobal <niko.escobal@gmail.com>
2021-10-15 23:33:23 +0800

    Revert "Add function to display images dynamically"
    This reverts commit f2c51c7fda543c0835e92c6b58ce692dc506bc74.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const url = 'https://api.artic.edu/api/v1/artworks'
-const artContent = document.createElement('div');
-const artContainer = document.getElementById('art-container');
+  const div = document.createElement('div');
+  const artContainer = document.getElementById('art-container');
+
+
 
 const getImages = async () => {
   const response = await fetch(url);
@@ -8,16 +10,16 @@ const getImages = async () => {
   const images = data.data;
 
   const imageString = images.map((image) => {
-      return {
-        id: image['id'],
-        image_id: image['image_id'],
-        title: image['title'],
-        date: image['date_start'],
-        artist: image['artist_title']
-      }
-    })
-    .filter(image => image.image_id !== null && image.title !== null)
-    .map(img => `<article
+    return {
+      id: image['id'],
+      image_id: image['image_id'],
+      title: image['title'],
+      date: image['date_start'],
+      artist: image['artist_title']
+    }
+  })
+  .filter(image => image.image_id !== null && image.title !== null)
+  .map(img =>  `<article
    class="flex justify-center w-full h-full p-3 flex-col space-y-2 border-double border-4 border-blue-300 bg-white">
    <h2 class="font-bold font-raleway text-lg">${img.title},
      ${img.date}</h2>
@@ -27,12 +29,12 @@ const getImages = async () => {
    <figure class="flex justify-between px-3 space-x-4">
      <figcaption class="flex py-3 space-x-6 text-base w-full font-nunito">
        <img id="${img.id}" src="/src/heart-filled.png" alt="heart icon">&nbsp; Like
-       <img id="${img.id}" src="/src/comment.png" alt="comment icon">&nbsp; Comment
+       <img id="${img.id} src="/src/comment.png" alt="comment icon">&nbsp; Comment
      </figcaption>
    </figure>
  </article>`).join('');
-  artContent.innerHTML = imageString;
-  artContainer.appendChild(artContent)
+  div.innerHTML = imageString;
+  artContainer.appendChild(div)
 
 }
 
