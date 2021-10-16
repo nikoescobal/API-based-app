2b252c037998b3118c16c68f5595c089aaeecd0c
Niko Escobal <niko.escobal@gmail.com>
2021-10-16 16:19:31 +0800

    Fix grid styling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const url = 'https://api.artic.edu/api/v1/artworks';
 const artContent = document.createElement('div');
 const artContainer = document.getElementById('art-container');
-artContent.classList.add('w-full', 'grid', 'grid-cols-2', 'p-6', 'gap-4');
+artContent.classList.add('art-style');
 
 const getImages = async () => {
   const response = await fetch(url);
@@ -9,19 +9,19 @@ const getImages = async () => {
   const images = data.data;
 
   const imageString = images.map((image) => ({
-    id: image.id,
-    image_id: image.image_id,
-    title: image.title,
-    date: image.date_start,
-    artist: image.artist_title,
-  }))
+      id: image.id,
+      image_id: image.image_id,
+      title: image.title,
+      date: image.date_start,
+      artist: image.artist_title,
+    }))
     .filter((image) => image.image_id !== null && image.title !== null)
     .map((img) => `<article
    class="flex justify-center w-full h-full p-3 flex-col space-y-2 border-double border-4 border-blue-300 bg-white">
    <h2 class="font-bold font-raleway text-lg">${img.title},
      ${img.date}</h2>
    <h3 class="px-3 font-extralight font-raleway">${img.artist}</h3>
-   <img class="h-4/5 object-cover" src="https://www.artic.edu/iiif/2/${img.image_id}/full/843,/0/default.jpg"
+   <img class="image-style" src="https://www.artic.edu/iiif/2/${img.image_id}/full/843,/0/default.jpg"
      alt="image of artwork">
    <figure class="flex justify-between px-3 space-x-4">
      <figcaption class="flex py-3 space-x-6 text-base w-full font-nunito">
