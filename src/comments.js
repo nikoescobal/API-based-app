b1cc26a7ca8c1f67730224106c730a8812f1243f
Niko Escobal <niko.escobal@gmail.com>
2021-10-17 14:55:52 +0800

    Add comment popup, fix styling, add tests

--- src/comments.js
+++
@@ -1,1 +0,0 @@
-// comments code functions
--- src/index.js
+++ src/index.js
@@ -1,15 +1,156 @@
-// import article, {
-//   updateLikes,
-//   getComment,
-//   createMovieComment,
-//   commentGetter
-// } from '/src/components/art.js'
+const grab = (e) => document.getElementById(e);
 
-const url = 'https://api.artic.edu/api/v1/artworks?limit=20';
+const url = 'https://api.artic.edu/api/v1/artworks?limit=17';
 const artContent = document.createElement('div');
 const artContainer = document.getElementById('art-container');
+const popupContainer = document.querySelector('.bg-popup');
+const commentForm = document.querySelector('form');
+const messageContainer = document.querySelector('.message');
+const commentsCounterParagraph = document.querySelector('.comments-counter');
 artContent.classList.add('art-style');
 
+const likes = [];
+let images = [];
+
+const message = (text) => {
+  messageContainer.innerHTML = text;
+  setTimeout(() => {
+    messageContainer.innerHTML = '';
+  }, 2000);
+};
+
+const updateLikes = async () => {
+  const appID = 'Y5ExZ6TMJ2KXP15dXk0s';
+  await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appID}/likes`)
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((article) => {
+        likes.push(article.likes);
+      });
+    });
+};
+
+const like = async (id) => {
+  const appID = 'Y5ExZ6TMJ2KXP15dXk0s';
+  await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appID}/likes/`, {
+    method: 'Post',
+    headers: {
+      'content-type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify({
+      item_id: id,
+    }),
+  }).then(() => {
+    updateLikes();
+  });
+};
+
+const renderComment = (arr) => {
+  const commentContainer = document.querySelector('.comments-container');
+  commentContainer.innerHTML = '';
+  if (arr) {
+    arr.forEach((item) => {
+      commentContainer.innerHTML += `
+      <span class="bg-blue-400 p-2 mt-3 text-lg text-white mr-0">${item.creation_date}</span>
+      <span class="bg-blue-500 p-2 mt-3 text-lg text-white">${item.username}:</span>
+      <span class="p-2 mt-3 text-lg">${item.comment}</span>
+      <br>
+      <br>`;
+    });
+  }
+};
+
+const fetchComments = async (id) => {
+  await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Y5ExZ6TMJ2KXP15dXk0s/comments?item_id=${id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.length > 0) {
+        commentsCounterParagraph.innerHTML = `Comment (${data.length})`;
+      }
+      renderComment(data);
+    });
+};
+
+const createNewComment = async (newObject, itemId = null) => {
+  const appID = 'Y5ExZ6TMJ2KXP15dXk0s';
+  await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appID}/comments/`, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; Charset=UTF-8',
+    },
+    body: JSON.stringify(newObject),
+  })
+    .then((response) => {
+      if (response.status === 201) {
+        message('Comment successfully added.');
+        fetchComments(itemId);
+      }
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
+};
+
+const getLikeElements = () => {
+  const hearts = document.querySelectorAll('.like');
+  const likeCounter = document.querySelectorAll('.like-count');
+  hearts.forEach((heart, index) => {
+    let counter = 0;
+    heart.addEventListener('click', (e) => {
+      e.preventDefault();
+      like(images[index].id);
+      counter += 1;
+      likeCounter[index].innerHTML = `${likes[index] + counter} Likes`;
+    });
+  });
+};
+
+const handleCommentForm = (itemId) => {
+  commentForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const name = grab('name');
+    const comment = grab('comment');
+
+    if (name.value && comment.value !== '') {
+      const newComment = {
+        item_id: itemId,
+        username: name.value,
+        comment: comment.value,
+      };
+      createNewComment(newComment, itemId);
+      name.value = '';
+      comment.value = '';
+    } else {
+      message('please fill all fields');
+    }
+  });
+};
+
+const openPopup = (images) => {
+  const trigger = document.querySelectorAll('.comment');
+  trigger.forEach((element, index) => {
+    element.addEventListener('click', () => {
+      popupContainer.classList.remove('hidden');
+
+      images.forEach(async (img, imgIndex) => {
+        if (imgIndex === index) {
+          grab('popup-image').src = `https://www.artic.edu/iiif/2/${img.image_id}/full/843,/0/default.jpg`;
+          fetchComments(img.id);
+          handleCommentForm(img.id);
+        }
+      });
+    });
+  });
+};
+
+const closePopup = () => {
+  const closeButton = grab('closeIcon');
+  closeButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    popupContainer.classList.add('hidden');
+  });
+};
 
 const getImages = async () => {
   const response = await fetch(url);
@@ -22,9 +163,10 @@ const getImages = async () => {
     title: image.title,
     date: image.date_start,
     artist: image.artist_title,
-  })).filter((image) => image.image_id !== null && image.artist !== null)
-  const imageString = images.map((img, index) => `<article
-      class="article-style">
+  })).filter((image) => image.image_id !== null && image.artist !== null);
+
+  const imageString = images.map((img, index) => `
+    <article class="article-style">
       <h2 class="title">${img.title},
         ${img.date}</h2>
       <h3 class="artist">${img.artist}</h3>
@@ -32,143 +174,23 @@ const getImages = async () => {
         alt="image of artwork">
       <figure class="caption-container">
         <figcaption class="caption-content">
-          <img class="like" id="${img.id}" src="/src/heart-empty.png" alt="like icon">&nbsp;<span class="like-count">${likes[index]} Likes</span>
+          <img class="like" id="${img.id}" src="/src/heart-empty.png" alt="like icon">&nbsp;
+            <span class="like-count">
+            ${likes[index]} Likes
+            </span>
           <img class="comment" id="${img.id}" src="/src/comment.png" alt="comment icon">&nbsp;<span class="comment-count">Comments</span>
         </figcaption>
       </figure>
     </article>`).join('');
   artContent.innerHTML = imageString;
   artContainer.appendChild(artContent);
+
   getLikeElements();
-
-  const popup = async (image_id, id, appID) => {
-    commentGetter(id, appID)
-    const commentForImages = getComment(id)
-    console.log(commentForImages);
-    grab('popup-image').src = image_id
-    grab('popup').classList.add('container')
-  };
-
-
-  const comentBtn = document.querySelectorAll('.comment')
-  comentBtn.forEach(element => {
-    element.addEventListener('click', () => {
-      // grab('popup').classList.add('container')
-      popup()
-    })
-  });
+  openPopup(images);
+  closePopup();
 };
 
-
-const grab = (e) => document.getElementById(e);
-const subBtn = document.querySelector('.sub')
-
-async function getComment(id, appID) {
-  await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Y5ExZ6TMJ2KXP15dXk0s/comments?item_id=${id}`)
-    .then(r => r.json())
-    .then(d => {
-      renderComment(d)
-    })
-}
-
-const commentGetter = (id, appID) => {
-  subBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    alert('hi' + id)
-    const name = grab('name')
-    const comment = grab('comment')
-    if (name.value === '' && comment.value === '') return
-    createMovieComment(id, name.value, comment.value, appID)
-    console.log(name.value, comment.value);
-    name.value = ''
-    comment.value = ''
-  })
-}
-
-const renderComment = (arr) => {
-  const commentContainer = grab('popComment')
-  commentContainer.innerHTML = '';
-  if (arr) {
-    arr.forEach((item) => {
-      commentContainer.innerHTML += `
-      <div>
-      <p>
-      ${item.creation_date} ${item.username}: ${item.comment}
-      </p>
-      </div>
-      <br>
-      `
-    })
-  }
-}
-
-async function like(id) {
-  const appID = 'Y5ExZ6TMJ2KXP15dXk0s';
-  await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appID}/likes/`, {
-    method: 'Post',
-    headers: {
-      'content-type': 'application/json; charset=UTF-8'
-    },
-    body: JSON.stringify({
-      item_id: id
-    })
-  }).then(() => {
-    updateLikes
-    console.log(id);
-  })
-}
-
-const likes = [];
-
-const updateLikes = async () => {
-  const appID = 'Y5ExZ6TMJ2KXP15dXk0s';
-  const likeCount = document.querySelectorAll('.like-count')
-  await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appID}/likes`)
-    .then(response => response.json())
-    .then((data) => {
-      data.forEach((article, index) => {
-        likes.push(article.likes)
-      })
-      getImages();
-    })
-}
-
-window.onload = updateLikes;
-
-function poop() {
-  console.log("poop")
-}
-
-let images = []
-/* eslint-disable */
-/* stylelint-disable */
-const getLikeElements = () => {
-  const hearts = document.querySelectorAll('.like');
-  const likeCounter = document.querySelectorAll('.like-count');
-  hearts.forEach((heart, index) => {
-    let counter = 0;
-    heart.addEventListener('click', (e) => {
-      like(images[index].id)
-      counter += 1;
-      likeCounter[index].innerHTML = `${likes[index]+counter} Likes`
-    });
-  });
+window.onload = () => {
+  getImages();
+  updateLikes();
 };
-/* eslint-enable */
-/* stylelint-enable */
-
-// async function createMovieComment(id, userName, userComment, appID) {
-//   const data = JSON.stringify({
-//     item_id: id,
-//     username: userName,
-//     comment: userComment
-//   });
-//   const resp = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appID}/comments/`, {
-//     method: 'POST',
-//     headers: {
-//       'Content-type': 'application/json; Charset=UTF-8',
-//     },
-//     body: data,
-//   });
-//   return resp.text();
-// };
